09a3599d9f7cd93a7d0c8f941377dc99e042ba0f
Tom Stine <git@13z.one>
2024-03-24 16:20:53 -0500

    removed import portfolio/page.tsx

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,5 @@
 // PORTFOLIO PAGE
 
-import FlipCard from "@/components/FlipCard";
-import Link from "next/link";
-import { MdiGithub } from "@/utils/icons";
-
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Project } from "@/types/allTypes";
@@ -17,18 +13,6 @@ export default function PortfolioPage() {
     <div className="w-full max-w-7xl flex flex-col justify-center items-center" >
       <Header />
       <Portfolio allProjects={allProjects} />
-      {/* <main className="flex flex-col justify-start items-center px-0">
-        <h1 className="pb-12 text-center uppercase text-zinc-400 font-mono text-2xl tracking-wide6 z-10">portfolio</h1>
-        <div className="w-full flex justify-center flex-wrap gap-8 z-20">
-          {allProjects.map((project: Project, index) => (
-            <FlipCard key={project.name} index={index} {...project} />
-          ))}
-        </div>
-        <Link href="https://github.com/13zebras?tab=repositories" className="mt-16 py-[7px] w-[250px] flex justify-center items-center gap-x-2 bg-zinc-900 hover:bg-zinc-800 active:bg-zinc-800 text-zinc-300 hover:text-zinc-200 active:text-zinc-100 text-sm rounded-2xl border border-zinc-600 hover:border-zinc-500 active:border-sky-500">
-          <MdiGithub />
-          Github Repositories
-        </Link>
-      </main> */}
       <Footer />
     </div>
   )
