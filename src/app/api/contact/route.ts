6b8f7cf82b7679d0e045db6893eabad8d7ab23db
Tom Stine <git@13z.one>
2024-04-09 00:49:00 -0500

    update for .env on vercel

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -9,47 +9,44 @@ import type Mail from 'nodemailer/lib/mailer'
 // Authentication:  PLAIN
 
 export async function POST(request: NextRequest) {
-  const { email, name, message } = await request.json()
+	const { email, name, message } = await request.json()
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.fastmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.FASTMAIL_EMAIL,
-      pass: process.env.FASTMAIL_PASSWORD_______REMOVE_ME__XXXXXXXXXXXXXX,
-    },
-  })
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.fastmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+			user: process.env.FASTMAIL_EMAIL,
+			pass: process.env.FASTMAIL_PASSWORD,
+		},
+	})
 
-  const mailOptions: Mail.Options = {
-    from: process.env.FASTMAIL_EMAIL,
-    to: 'contact13z.dev@13z.foo',
-    subject: 'RECEIVED: 13z.dev ContactForm',
-    text: `from: ${name}\nemail: ${email}\nmessage: ${message}`,
-  }
+	const mailOptions: Mail.Options = {
+		from: process.env.FASTMAIL_EMAIL,
+		to: 'contact13z.dev@13z.foo',
+		subject: 'RECEIVED: 13z.dev ContactForm',
+		text: `from: ${name}\nemail: ${email}\nmessage: ${message}`,
+	}
 
-  // console.log('>>>> mailOptions', mailOptions)
+	// console.log('>>>> mailOptions', mailOptions)
 
-  const sendMailPromise = () =>
-    new Promise<string>((resolve, reject) => {
-      transporter.sendMail(mailOptions, (err) => {
-        // console.log('>>>> transporter in promise')
-        if (!err)
-          resolve('Email sent')
-        else
-          reject(err.message)
-      })
-    })
+	const sendMailPromise = () =>
+		new Promise<string>((resolve, reject) => {
+			transporter.sendMail(mailOptions, (err) => {
+				// console.log('>>>> transporter in promise')
+				if (!err) resolve('Email sent')
+				else reject(err.message)
+			})
+		})
 
-  try {
-    await sendMailPromise()
-    // console.log('>>>> sendMailPromise returned\n****************\n\n')
+	try {
+		await sendMailPromise()
+		// console.log('>>>> sendMailPromise returned\n****************\n\n')
 
-    return NextResponse.json({ message: 'Email sent' })
-  }
-  catch (err) {
-    // console.log('>>>> sendMailPromise error', err, '\n****************\n\n')
+		return NextResponse.json({ message: 'Email sent' })
+	} catch (err) {
+		// console.log('>>>> sendMailPromise error', err, '\n****************\n\n')
 
-    return NextResponse.json({ error: err }, { status: 500 })
-  }
+		return NextResponse.json({ error: err }, { status: 500 })
+	}
 }
