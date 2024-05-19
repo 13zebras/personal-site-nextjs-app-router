0171f947f70226445f3d4bc4f7b26d5f229d14bc
Tom Stine <git@13z.one>
2024-05-18 17:30:40 -0700

    updated config to use biome

--- next.config.js
+++ next.config.js
@@ -3,20 +3,6 @@
 // const nextConfig = {
 module.exports = {
 	reactStrictMode: true,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'picsum.photos',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'placehold.co',
-				pathname: '/**',
-			},
-		],
-	},
 }
 
 // module.exports = nextConfig
